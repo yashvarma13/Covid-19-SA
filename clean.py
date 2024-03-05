4eddd89a4eac07c2432919cb3c78db67259cfdbc
awesomecode <ankithh354@gmail.com>
2024-03-05 21:38:20 +0530

    Removed Dataset

--- clean.py
+++ clean.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
 
-tweets = pd.read_csv("Corona_tweets.csv")
+tweets = pd.read_csv("Corona_tweets.csv",encoding="utf-8")
 
-print(tweets.head())  # Number of
+print(tweets.head(5))  # Number of
